58179acb1ccc3968fcb682d2a3804916f745b96f
Harsh Singh <harsh945455@gmail.com>
2025-01-03 20:30:11 +0530

    Add files via upload

---
+++ controllers/adminController.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+//API for adding doctor
+const addDoctor=async(requestAnimationFrame,res)=>{
+    try{
+const{name,email,password,speciality,degree,experience,about,fees,address} = req.body
+const imageFile=req.file
+
+console.log({name,email,password,speciality,degree,experience,about,fees,address},imageFile);
+    } catch (error){
+
+    }
+}
+export {addDoctor}
